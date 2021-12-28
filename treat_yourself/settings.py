f5474f970f76e042b416dbd2464b5ceeaa45066c
Maya Sharma <mayasharma95@outlook.com>
2021-12-28 13:05:01 +0000

    Removed secret key

--- treat_yourself/settings.py
+++ treat_yourself/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4p6(tpq!+14eto@4f#pl0=z_a)0!fn#6&zf_!-n3bvyt!ft+in'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['treat-yourself-maya95.herokuapp.com', 'localhost']
 
