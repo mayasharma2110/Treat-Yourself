f6ea261764722fe6b8c57fe8ef1618ecac017358
Maya Sharma <mayasharma95@outlook.com>
2022-01-05 13:58:05 +0000

    Completed confirmation emails

--- treat_yourself/settings.py
+++ treat_yourself/settings.py
@@ -184,6 +184,7 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'treatyourself@example.com'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
