22ae5b2f158b8a40ad1c9956b979cd194bc74123
Maya Sharma <mayasharma95@outlook.com>
2022-01-05 13:29:37 +0000

    Finished basic user profile page

--- profiles/views.py
+++ profiles/views.py
@@ -7,6 +7,7 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
+@login_required
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -19,12 +20,29 @@ def profile(request):
         else:
             messages.error(request, 'Failed to update profile. Please ensure the form is valid.')
     else:
-        form = UserProfileForm(request.POST, instance=profile)
+        form = UserProfileForm(instance=profile)
 
     orders = profile.orders.all()
     template = 'profiles/profile.html'
     context = {
         'form': form,
+        'orders': orders,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
     }
 
     return render(request, template, context)
