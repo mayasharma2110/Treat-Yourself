7bd756fd095e780361c9d6dd62c2803fa8a69d7b
Maya Sharma <mayasharma95@outlook.com>
2022-01-10 19:02:34 +0000

    Updates due to PEP8

--- products/models.py
+++ products/models.py
@@ -37,7 +37,7 @@ class Product(models.Model):
 class ProductReview(models.Model):
     user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
                                      null=True,
-                                     blank=True, 
+                                     blank=True,
                                      related_name="reviews")
     product = models.ForeignKey(Product, null=False, blank=False,
                                 on_delete=models.CASCADE,
--- products/views.py
+++ products/views.py
@@ -1,12 +1,12 @@
+from decimal import Decimal
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from decimal import Decimal
 
 from django.db.models import Q
 from django.db.models.functions import Lower
 
-from .models import Product, Category, ProductReview
+from .models import Product, Category
 from .forms import ProductForm
 
 
