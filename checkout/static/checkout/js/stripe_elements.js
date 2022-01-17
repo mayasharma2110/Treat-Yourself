d3e6818c91a6162668259a5c7840143fa0b5be4d
Maya Sharma <mayasharma95@outlook.com>
2022-01-17 20:00:15 +0000

    Updated due to some jshint issues

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -122,5 +122,5 @@ form.addEventListener('submit', function(ev) {
         // If anything goes wrong posting the data to our view. We'll reload the page and display the error without ever charging the user.
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
